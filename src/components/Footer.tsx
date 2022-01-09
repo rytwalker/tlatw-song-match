31aae22138266dc3057f7140bc55e67dd0a23e8a
Ryan Walker <rytwalker@gmail.com>
2022-01-09 17:15:57 -0500

    add website to footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -5,6 +5,7 @@ function Footer() {
   return (
     <StyledFooter>
       <SocialNav />
+      <WebsiteLink href="thelighthouseandthewhaler.com" rel="nofollow noref">thelighthouseandthewhaler.com</WebsiteLink>
       <div>© 2020 The Lighthouse and the Whaler</div>
     </StyledFooter>
   );
@@ -14,6 +15,13 @@ const StyledFooter = styled.footer`
   width: 100%;
   text-align: center;
   padding: 30px;
+`;
+
+const WebsiteLink = styled.a`
+  color: white;
+  display: inline-block;
+  margin-bottom: 16px;
+  text-decoration: none;
 `;
 
 export default Footer;
